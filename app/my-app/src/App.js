b62f1245886afcba818645c63d718b1b3b47abd1
passat988 <107483813+passat988@users.noreply.github.com>
2022-08-29 23:31:18 +0300

    first app/first commit

---
+++ app/my-app/src/App.js
@@ -0,0 +1,51 @@
+import React, { useMemo, useState, useRef } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/counter";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/buttons/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/UI/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false)
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Создать пользователя</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Список постов 1"
+      />
+    </div>
+  );
+}
+
+export default App;
